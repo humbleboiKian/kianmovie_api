c82b2369369789671be8582de39971f95633f958
humbleboikian <yazar.h67@rsu.ac.th>
2026-04-25 15:50:56 +0700

    up

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ app.use(express.json());
 
 const movieRoutes = require("./routes/movies");
 const authRoutes = require("./routes/auth");
+const usersRoutes = require("./routes/users"); // ✅ ADD THIS
+
 app.use("/api/auth", authRoutes);
 app.use("/api/movies", movieRoutes);
+app.use("/api/users", usersRoutes); // ✅ ADD THIS
 
 app.get("/", (req, res) => {
   res.send("Movie API is running");
