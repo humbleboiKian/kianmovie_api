055d7da6f0a0f2fc9d339bbca05d12073a7b6d03
humbleboikian <yazar.h67@rsu.ac.th>
2026-04-25 15:37:44 +0700

    api

--- api/movies.js
+++ api/movies.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("./../db");
+const db = require("../db");
 
 // 1. GET all movies (already working)
 router.get("/", (req, res) => {
